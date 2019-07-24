14ff87c3b2381686c780efcc25acb8fa22a0a1a6
Solrac5555 <53198843+Solrac5555@users.noreply.github.com>
2019-07-24 17:07:45 -0500

    Update index.php

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ $appName = explode('.', $domain)[0];
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width,initial-scale=1">
-        <meta name="description" content="Official PHP SDK for Mercado Libre's API.">
+        <meta name="description" content="My PHP SDK for Mercado Libre's API.">
         <meta name="keywords" content="API, PHP, Mercado Libre, SDK, meli, integration, e-commerce">
         <title>Mercado Libre PHP SDK</title>
         <link rel="stylesheet" href="/getting-started/style.css" />
@@ -22,7 +22,7 @@ $appName = explode('.', $domain)[0];
 
     <body>
         <header class="navbar">
-            <a class="logo" href="#"><img src="/getting-started/logo-developers.png" alt=""></a>
+            <a class="logo" href="#"><img src="https://www.ingeniatv.com/wp-content/uploads/2018/07/IngTv9.png" alt=""></a>
             <nav>
                 <ul class="nav navbar-nav navbar-right">
                     <li><a target="_blank" href="http://developers.mercadolibre.com/getting-started/">Getting Started</a></li>
@@ -34,13 +34,13 @@ $appName = explode('.', $domain)[0];
 
         <div class="header">
             <div>
-                <h1>Getting Started with Mercado Libre's PHP SDK</h1>
-                <h2>Official PHP SDK for Mercado Libre's API.</h2>
+                <h1>API ML PHP SDK</h1>
+                <h2>My PHP SDK for Mercado Libre's API.</h2>
             </div>
         </div>
 
         <main class="container">
-            <h3>Hi, Developer!</h3>
+            <h3>Hola,</h3>
             <p>This is a sample app, deployed to Heroku with Mercado Libre's PHP SDK. Feel free to use it as a base, to start building your awesome app!</p>
 
             <div class="row">
