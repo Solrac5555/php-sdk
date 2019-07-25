49d590f49f430723c5536ead82b5986995b82c47
Solrac5555 <53198843+Solrac5555@users.noreply.github.com>
2019-07-25 17:01:31 -0500

    Update index.php

--- index.php
+++ index.php
@@ -129,23 +129,23 @@ $appName = explode('.', $domain)[0];
                      <br />
                     </p>
                     <pre class="pre-item">
-"title" => "Item De Teste - Por Favor, Não Ofertar! --kc:off",
-        "category_id" => "MLA1430",
+				"title" => "Item de prueba favor no ofertar! --kc:off",
+        "category_id" => "MLA1051",
         "price" => 10,
-        "currency_id" => "MXN",
+        "currency_id" => "MEX",
         "available_quantity" => 1,
         "buying_mode" => "buy_it_now",
         "listing_type_id" => "bronze",
         "condition" => "new",
-        "description" => "Item de Teste. Mercado Livre's PHP SDK.",
+        "description" => array ("plain_text" => "Item de Teste. Mercado Livre's PHP SDK."),
         "video_id" => "RXWn6kftTHY",
         "warranty" => "12 month",
         "pictures" => array(
             array(
-                "source" => "https://upload.wikimedia.org/wikipedia/commons/f/fd/Ray_Ban_Original_Wayfarer.jpg"
+                "source" => "https://upload.wikimedia.org/wikipedia/commons/thumb/6/64/IPhone_7_Plus_Jet_Black.svg/440px-IPhone_7_Plus_Jet_Black.svg.png"
             ),
             array(
-                "source" => "https://upload.wikimedia.org/wikipedia/commons/a/ab/Teashades.gif"
+                "source" => "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bc/IPhone7.jpg/440px-IPhone7.jpg"
             )
         )
     )
@@ -164,32 +164,86 @@ $appName = explode('.', $domain)[0];
                             try {
                                 print_r($meli->refreshAccessToken());
                             } catch (Exception $e) {
-                                echo "Exception: ",  $e->getMessage(), "\n";
+                                echo "Exception: ",  $e->getMessage(), "Salto de line \n";
                             }
                         }
                         // We construct the item to POST
-                        $item = array(
-                            "title" => "Articulo de prueba favor no ofertar! --kc:off",
-        "category_id" => "MLA1430",
+						
+						$item = array(
+		"title" => "Item de prueba favor no ofertar! --kc:off",
+        "category_id" => "MLA1051",
         "price" => 10,
-        "currency_id" => "MXN",
+        "currency_id" => "MEX",
         "available_quantity" => 1,
         "buying_mode" => "buy_it_now",
         "listing_type_id" => "bronze",
         "condition" => "new",
-        "description" => "Item de Teste. Mercado Livre's PHP SDK.",
+        "description" => array ("plain_text" => "Item de Teste. Mercado Livre's PHP SDK."),
         "video_id" => "RXWn6kftTHY",
         "warranty" => "12 month",
         "pictures" => array(
             array(
-                "source" => "https://upload.wikimedia.org/wikipedia/commons/f/fd/Ray_Ban_Original_Wayfarer.jpg"
+                "source" => "https://upload.wikimedia.org/wikipedia/commons/thumb/6/64/IPhone_7_Plus_Jet_Black.svg/440px-IPhone_7_Plus_Jet_Black.svg.png"
             ),
             array(
-                "source" => "https://upload.wikimedia.org/wikipedia/commons/a/ab/Teashades.gif"
+                "source" => "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bc/IPhone7.jpg/440px-IPhone7.jpg"
+            )
+        ),
+        "attributes" => array(
+            array(
+                "id" => "EAN",
+                "value_name" => "190198043566"
+            ),
+            array(
+                "id" => "COLOR",
+                "value_id" => "52049"
+            ),
+            array(
+                "id" => "WEIGHT",
+                "value_name" => "188g"
+            ),
+            array(
+                "id" => "SCREEN_SIZE",
+                "value_name" => "4.7 polegadas"
+            ),
+            array(
+                "id" => "TOUCH_SCREEN",
+                "value_id" => "242085"
+            ),
+            array(
+                "id" => "DIGITAL_CAMERA",
+                "value_id" => "242085"
+            ),
+            array(
+                "id" => "GPS",
+                "value_id" => "242085"
+            ),
+            array(
+                "id" => "MP3",
+                "value_id" => "242085"
+            ),
+            array(
+                "id" => "OPERATING_SYSTEM",
+                "value_id" => "296859"
+            ),
+            array(
+                "id" => "OPERATING_SYSTEM_VERSION",
+                "value_id" => "iOS 10"
+            ),
+            array(
+                "id" => "DISPLAY_RESOLUTION",
+                "value_id" => "1920 x 1080"
+            ),
+            array(
+                "id" => "BATTERY_CAPACITY",
+                "value_name" => "3980 mAh"
+            ),
+            array(
+                "id" => "FRONT_CAMERA_RESOLUTION",
+                "value_name" => "7 mpx"
             )
         )
-    );
-                        
+    );                    
                         $response = $meli->post('/items', $item, array('access_token' => $_SESSION['access_token']));
                         // We call the post request to list a item
                         echo "<h4>Response</h4>";
